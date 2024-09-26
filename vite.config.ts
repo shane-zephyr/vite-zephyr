c6d5cd3a37f90815b415176b6de880d1579871ff
Shane <shane@swalker.dev>
2024-09-26 18:01:59 -0500

    Base router imple

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { withZephyr } from "vite-plugin-zephyr";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), withZephyr()],
+	plugins: [react(), withZephyr(), TanStackRouterVite()],
 });
